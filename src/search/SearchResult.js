a832dcdd1a88c248d30d5501d6d53734609b627a
Bartosz Pindel <bartos.pindel@gmail.com>
2022-12-12 14:22:45 +0100

    p

--- src/search/SearchResult.js
+++ src/search/SearchResult.js
@@ -1,4 +1,4 @@
-import { Fragment, useContext, useEffect } from "react";
+import { Fragment, useContext, useEffect, useState } from "react";
 import ResultContext from "../store/result-context";
 
 import classes from "./SearchResult.module.css";
@@ -6,56 +6,67 @@ import classes from "./SearchResult.module.css";
 const SearchResult = () => {
   const resultCtx = useContext(ResultContext);
 
-  // const resultName = resultCtx.name;
+  // const charackter = (
+  //   <Fragment>
+  //     <div className={classes.name}>
+  //       <h3>Name:</h3>
+  //       <p>Test name</p>
+  //     </div>
+  //     {/* <img alt="character image" /> */}
+  //     <p className={classes.question}>Status:</p>
+  //     <p className={classes.answer}>Test status</p>
+  //     <p className={classes.question}>Location:</p>
+  //     <p className={classes.answer}>Test location</p>
+  //     <p className={classes.question}>How many episodes they were in?</p>
+  //     <p className={classes.answer}>Test number</p>
+  //   </Fragment>
+  // );
 
-  // useEffect(() => {
-  //   // resultName;
-  //   console.log("ta");
-  // }, [resultCtx.name]);
+  // const location = (
+  //   <section>
+  //     <h3>Name</h3>
+  //     <p>Type</p>
+  //     <p>Dimension</p>
+  //     <p>How many residents there have been seen last?</p>
+  //   </section>
+  // );
 
-  const charackter = (
-    <section className={classes.section}>
-      <div className={classes.name}>
-        <h3>Name:</h3>
-        <p>Test name</p>
-      </div>
-      {/* <img alt="character image" /> */}
-      <p className={classes.question}>Status:</p>
-      <p className={classes.answer}>Test status</p>
-      <p className={classes.question}>Location:</p>
-      <p className={classes.answer}>Test location</p>
-      <p className={classes.question}>How many episodes they were in?</p>
-      <p className={classes.answer}>Test number</p>
-    </section>
-  );
-
-  const location = (
-    <section>
-      <h3>Name</h3>
-      <p>Type</p>
-      <p>Dimension</p>
-      <p>How many residents there have been seen last?</p>
-    </section>
-  );
-
-  const episode = (
-    <section>
-      <h3>Name</h3>
-      <p>Air date</p>
-      <p>Code of episode</p>
-      <p>How many charackters were in it?</p>
-    </section>
-  );
+  // const episode = (
+  //   <section>
+  //     <h3>Name</h3>
+  //     <p>Air date</p>
+  //     <p>Code of episode</p>
+  //     <p>How many charackters were in it?</p>
+  //   </section>
+  // );
+  // if (resultCtx.results) {
+  // setResults([...resultCtx.results]);
+  // }
+  // console.log(results === false);
 
   return (
     <div className={classes.sections}>
-      {charackter}
-      {charackter}
-      {charackter}
-      {charackter}
-      {charackter}
-      {/* {!resultCtx.results && <p>Start searching!</p>}
-      {resultCtx.results && resultCtx.results.map(() => charackter)} */}
+      {!resultCtx.results && <p>Start searching!</p>}
+      {resultCtx.results &&
+        resultCtx.results.map((result) => {
+          return (
+            <section className={classes.section} key={result.id}>
+              <div className={classes.name}>
+                <h3>Name:</h3>
+                <p>{result.name}</p>
+              </div>
+              {/* <img alt="character image" /> */}
+              <p className={classes.question}>Status:</p>
+              <p className={classes.answer}>{result.status}</p>
+              <p className={classes.question}>Location:</p>
+              <p className={classes.answer}>{result.location.name}</p>
+              <p className={classes.question}>
+                How many episodes they were in?
+              </p>
+              <p className={classes.answer}>{result.episode.length}</p>
+            </section>
+          );
+        })}
     </div>
   );
 };
